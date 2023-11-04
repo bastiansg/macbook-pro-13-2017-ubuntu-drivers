1ebd14c2efccd9f6a1964ad2787d42d7131a18cc
Bas <bastiansg.bas@gmail.com>
2023-11-04 14:50:44 -0300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+cd bcwc_pcie && make && sudo make install && rm facetimehd.mod
+cd ../facetimehd-firmware && sudo ./facetimehd-firmware-install.sh
+cd ../macbook12-bluetooth-driver && sudo ./install.bluetooth.sh
+cd ../snd_hda_macbookpro && sudo ./install.cirrus.driver.sh
