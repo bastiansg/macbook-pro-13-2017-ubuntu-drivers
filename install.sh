221ca122b7784816cf8d46663687ea6d562b804a
Bas <bastiansg.bas@gmail.com>
2024-09-17 12:04:47 -0300

    upgrade drivers

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cd bcwc_pcie && make && sudo make install && rm facetimehd.mod
 cd ../facetimehd-firmware && sudo ./facetimehd-firmware-install.sh
-cd ../macbook12-bluetooth-driver && sudo ./install.bluetooth.sh
+cd ../macbook12-bluetooth-driver && rm -r build && sudo ./install.bluetooth.sh
 cd ../snd_hda_macbookpro && sudo ./install.cirrus.driver.sh
